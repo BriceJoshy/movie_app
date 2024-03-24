c191a3c53853ddb7cd49c913b052d38fa52eb065
BRICE JOSHY <bricejoshy99@gmail.com>
2024-03-24 18:40:01 +0530

    adding the css file for the movie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useEffect } from "react";
+
+// API KEY - 15efdb82
+
+const API_URL = "http://www.omdbapi.com?apikey=15efdb82";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    // after response we need to get the data from it
+    const data = await response.json();
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Superman");
+  }, []);
   return <h1>Hello world</h1>;
 };
 
