c470618660f03e8a6676e0b779969b7c4968fb19
BRICE JOSHY <bricejoshy99@gmail.com>
2024-03-24 18:25:27 +0530

    initial app model

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-  const name = null;
-  const isNameShowing = false;
-  return (
-    <div className="App">
-      <h1>annachi {isNameShowing ? name : "someone"}</h1>
-
-      {name ? (
-        <>test1</>
-      ) : (
-        <>
-          <h1>test2</h1>
-          <h1>test2</h1>
-        </>
-      )}
-    </div>
-  );
+  return <h1>Hello world</h1>;
 };
 
 export default App;
